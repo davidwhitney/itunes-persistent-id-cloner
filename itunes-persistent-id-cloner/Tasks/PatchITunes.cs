db0a167f63fdb5200143c6bf07b9f09ff296d8ee
David Whitney <david@davidwhitney.co.uk>
2012-01-24 23:58:31 +0000

    prompts for input before patching.

--- itunes-persistent-id-cloner/Tasks/PatchITunes.cs
+++ itunes-persistent-id-cloner/Tasks/PatchITunes.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using itunes_persistent_id_cloner.Core;
 
 namespace itunes_persistent_id_cloner.Tasks
@@ -11,6 +12,14 @@ namespace itunes_persistent_id_cloner.Tasks
             Console.Write("Please enter your previously extracted Id: ");
 
             var persistentId = args.Length == 2 ? args[1] : Console.ReadLine().Trim();
+
+            Console.WriteLine("About to patch iTunes to persistent Id: "  + persistentId);
+
+            if (!args.ToList().Contains("-quiet"))
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadLine();
+            }
 
             var adapter = new TunesLibraryAdapter();
             adapter.PatchLibraryToPersistentId(persistentId);
