23fd177db7f483def1e75c706e9eb9df9ce5d8d0
David Whitney <david@davidwhitney.co.uk>
2012-01-25 00:04:09 +0000

    "cleaner". Ish.

--- itunes-persistent-id-cloner/Tasks/PatchITunes.cs
+++ itunes-persistent-id-cloner/Tasks/PatchITunes.cs
@@ -9,20 +9,54 @@ namespace itunes_persistent_id_cloner.Tasks
         public void Execute(string[] args)
         {
             Console.WriteLine("Please make sure iTunes is not running.");
-            Console.Write("Please enter your previously extracted Id: ");
 
-            var persistentId = args.Length == 2 ? args[1] : Console.ReadLine().Trim();
+            var persistentId = GetIdToPatchTo(args);
+            PromptForConfirmation(args, persistentId);
+            ThrowIfUserIsCripplinglyStupid(persistentId);
+            Patch(persistentId);
+        }
 
-            Console.WriteLine("About to patch iTunes to persistent Id: "  + persistentId);
-
-            if (!args.ToList().Contains("-quiet"))
-            {
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadLine();
-            }
-
+        private static void Patch(string persistentId)
+        {
             var adapter = new TunesLibraryAdapter();
             adapter.PatchLibraryToPersistentId(persistentId);
+        }
+
+        private static void ThrowIfUserIsCripplinglyStupid(string persistentId)
+        {
+            if (string.IsNullOrWhiteSpace(persistentId))
+            {
+                throw new InvalidOperationException("That's not a valid persistent Id. Type better.");
+            }
+        }
+
+        private static void PromptForConfirmation(string[] args, string persistentId)
+        {
+            Console.WriteLine();
+            Console.WriteLine("About to patch iTunes to persistent Id: " + persistentId);
+
+            if (args.ToList().Contains("-quiet"))
+            {
+                return;
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+
+        private static string GetIdToPatchTo(string[] args)
+        {
+            var persistentId = string.Empty;
+            if (args.Length > 1)
+            {
+                persistentId = args[1].Trim();
+            }
+            else
+            {
+                Console.Write("Please enter your previously extracted Id: ");
+                Console.ReadLine().Trim();
+            }
+            return persistentId;
         }
     }
 }
